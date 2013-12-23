c64e2947277cbe70b8b24d2df0fa97fdf428f4e1
bfhuang <bfhuang@thoughtworks.com>
2013-12-23 17:19:00 +0800

    add the .gitignore file to ignore the files that do not need to be committed

--- src/test/java/mvc/HelloControllerTest.java
+++ src/test/java/mvc/HelloControllerTest.java
@@ -1,39 +1,20 @@
 package mvc;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.ui.ModelMap;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration("file:/src/main/java/webapp/WEB-INF/spring-mvc-servlet.xml")
 public class HelloControllerTest {
-    private MockMvc mockMvc;
-    @Autowired
-    protected WebApplicationContext wac;
 
-    @Before
-    public void setup() {
-        this.mockMvc = webAppContextSetup(this.wac).build();
-    }
+	@Test
+	public void shouldReturnRightViewAndModel() throws Exception {
+		ModelMap model = new ModelMap();
+		String returnView = new HelloController().printWelcome(model);
 
-    @Test
-    public void simple() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("hello"))
-                .andExpect(forwardedUrl("/WEB-INF/pages/hello.jsp"))
-                .andExpect(model().attribute("message", "Hello world!"));
-    }
+		assertThat(returnView, is("hello"));
+		assertThat(model.get("message").toString(), is("Hello world!"));
 
+	}
 }
